7088d132d6964ae256f6a11e4a6fbec4879709f8
Abi Tyas Tunggal <abi@tyastunggal.com>
2019-07-08 11:29:34 +1000

    Update tailwind config

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,6 @@
-var tailwindcss = require('tailwindcss')
-
 module.exports = {
   plugins: [
-    tailwindcss('./app/javascript/stylesheets/tailwind.config.js'),
+    require('tailwindcss')
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
