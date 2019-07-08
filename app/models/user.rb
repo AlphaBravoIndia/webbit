a39864ed80d257a592094e8d166d344cadfa76ab
Abi Tyas Tunggal <abi@tyastunggal.com>
2019-07-08 22:51:48 +1000

    Remove username from Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,4 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :submissions
-  validates_uniqueness_of :username
-  validates_presence_of :username
 end
