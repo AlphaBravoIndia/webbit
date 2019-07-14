b40c8037aad39035ef28bce5336c4ee646db1ee7
Abi Tyas Tunggal <abi@tyastunggal.com>
2019-07-14 19:50:00 +1000

    Add communities

---
+++ db/migrate/20190714094358_add_community_ref_to_submissions.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class AddCommunityRefToSubmissions < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :submissions, :community, foreign_key: true
+  end
+end
---
+++ db/migrate/20190714094625_add_user_ref_to_communities.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class AddUserRefToCommunities < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :communities, :user, foreign_key: true
+  end
+end
